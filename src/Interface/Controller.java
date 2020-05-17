255d24ce75da6df8a75c95f23fb4db114696ee59
kellydena <49529455+kellydena@users.noreply.github.com>
2020-05-17 19:04:00 -0300

    controller

--- src/Interface/Controller.java
+++ src/Interface/Controller.java
@@ -1,6 +1,6 @@
 package Interface;
 
-
+import java.util.Scanner;
 
 public class Controller {
 	
@@ -17,14 +17,22 @@ public class Controller {
 		while(resposta != Acao.SAIR) {
 			resposta = view.getAcao();
 			System.out.println("Sua escolha foi " + resposta);
-			if(resposta==Acao.RAIZ) {
-				InstanciaRaiz inst = view.getInstanciaRaiz();
-				double numero = inst.getNumero();
-				double indice = inst.getIndice();
-				System.out.println("numero = " + inst.getNumero());
-				System.out.println("indice = " + inst.getIndice());
-				System.out.println("raiz de " + numero + " com indice " + indice + 
-						" eh " + Math.pow(numero, 1.0/indice));
+			
+			if(resposta==Acao.alunos) {
+				
+			}
+			if(resposta==Acao.cursos) {
+				
+			}
+			if(resposta == Acao.limparHistorico) {
+				
+			}
+			if(resposta == Acao.redimento) {
+				
+			}if(resposta == Acao.incluirNovoAluno) {
+				
+			}if(resposta == Acao.novoCurso) {
+				
 			}
 		}
 		
