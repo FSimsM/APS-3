a6c162faf2e135cefdc7b3b25914949a05000864
kellydena <49529455+kellydena@users.noreply.github.com>
2020-05-18 19:03:48 -0300

    leitor de cursos

--- src/entidade/Curso.java
+++ src/entidade/Curso.java
@@ -1,5 +1,36 @@
 package entidade;
 
 public class Curso {
+	String nome;
+	String tipo;
+	String ano;
+	public Curso(String nome, String tipo, String ano) {
+		this.nome = nome;
+		this.tipo = tipo;
+		this.ano = ano;
+	}
+	public String getNome() {
+		return nome;
+	}
+	
+	public String getTipo() {
+		return tipo;
+	}
+	
+	public String getAno() {
+		return ano;
+	}
+	
+	@Override
+	public String toString() {
+		String res = "";
+		res += "Nome: " + nome + "\n";
+		res += "Tipo: " + tipo + "\n";
+		res += "Ano: " + ano + "\n";
+		return res;
+				
+	}
+	
+	
     
 }
---
+++ src/leituraCSV/LeitorCursos.java
@@ -0,0 +1,44 @@
+package leituraCSV;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+import entidade.Aluno;
+import entidade.Curso;
+
+public class LeitorCursos {
+	public static String path = "filesCSV/cursos.csv";
+
+	public static List<Curso> getCursos() {
+
+		List<Curso> cursos = new ArrayList<>();
+
+		// try-with-resource
+		try (BufferedReader in = new BufferedReader(
+				new InputStreamReader(new FileInputStream(path), StandardCharsets.UTF_8))) {
+			String linha;
+			while ((linha = in.readLine()) != null) {
+				String[] palavras = linha.split(";");
+
+				String nome = palavras[0];
+				String tipo = palavras[1];
+				String ano = palavras[2];
+
+				Curso curso = new Curso(nome, tipo, ano);
+
+				cursos.add(curso);
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return cursos;
+
+	}
+}
