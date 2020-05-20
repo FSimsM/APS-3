8bbb4110a9ed920985ae39d5a054cc8028528a68
kellydena <49529455+kellydena@users.noreply.github.com>
2020-05-19 22:41:25 -0300

    Domain Exception

---
+++ src/controle/erro/DomainException.java
@@ -0,0 +1,12 @@
+package controle.erro;
+
+public class DomainException extends Exception{
+	
+	private static final long serialVersionUID = 1L;
+
+	public DomainException(String msg) {
+		super(msg);
+	}
+	
+
+}
