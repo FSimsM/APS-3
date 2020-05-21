585d26adab3812293dcec3e2fa879f4cae8fa612
thiagoklemg <thiagoklemg@gmail.com>
2020-05-21 20:25:38 -0300

    update do da kelly

--- src/leituraCSV/NovoCurso.java
+++ src/leituraCSV/NovoCurso.java
@@ -7,41 +7,42 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class NovoCurso {
-	Scanner sc = new Scanner(System.in);
-	public String path = "filesCSV/cursos.csv";
-	File arquivo = new File(path);
+    Scanner sc = new Scanner(System.in);
+    public String path = "filesCSV/cursos.csv";
+    File arquivo = new File(path);
 
-	public void novoCurso() {
-	
-		System.out.println("Digite o nome do curso");
-		String nome = sc.next();
-		System.out.println("Digite o nivel do curso (POS_GRADUACAO ou GRADUACAO)");
-		String nivel = sc.next();
-		System.out.println("Digite o ano do curso");
-		String ano = sc.next();
+    public void novoCurso() {
 
-		if (nivel.equals("GRADUACAO")) {
-			arquivo = new File("filesCSV/GraduacaoCSV/" + nome + "_" + nivel + "_" + ano + ".csv");
-		} else if (nivel.equals("POS_GRADUACAO")) {
-			arquivo = new File("filesCSV/PosGraduacaoCSV/" + nome + "_" + nivel + "_" + ano + ".csv");
-		}
-		try {
-			arquivo.createNewFile();
-		} catch (IOException e) {
+        System.out.println("Digite o nome do curso");
+        String nome = sc.next();
+        System.out.println("Digite o nivel do curso (POSGRADUACAO ou GRADUACAO)");
+        String nivel = sc.next();
+        System.out.println("Digite o ano do curso");
+        String ano = sc.next();
 
-		}
+        if (nivel.equals("GRADUACAO")) {
+            arquivo = new File("filesCSV/GraduacaoCSV/" + nome + "" + nivel + "_" + ano + ".csv");
+        } else if (nivel.equals("POSGRADUACAO")) {
+            arquivo = new File("filesCSV/PosGraduacaoCSV/" + nome + "" + nivel + "_" + ano + ".csv");
+        }
+        try {
+            arquivo.createNewFile();
+        } catch (IOException e) {
 
-		String[] lines = new String[] { nome + ";" + nivel + ";" + ano };
+            }
 
-		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {
-			for (String line : lines) {
-				bw.write(line);
-				bw.newLine();
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+            String[] lines = new String[] { nome + ";" + nivel + ";" + ano };
 
-	}
+            try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {
+                for (String line : lines) {
+                    bw.write(line);
+                    bw.newLine();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (DomainException r) {
+            System.out.println(r.getMessage());
+        }
+    }
 
-}
