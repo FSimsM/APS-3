c47945cdeb0f8de1ca8e0e00355236565ec78e21
kellydena <49529455+kellydena@users.noreply.github.com>
2020-05-22 15:15:35 -0300

    Controle de erro novoCurso

--- src/leituraCSV/NovoCurso.java
+++ src/leituraCSV/NovoCurso.java
@@ -7,56 +7,54 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Scanner;
 
+import controle.erro.DomainException;
 import entidade.Curso;
 
 public class NovoCurso {
-    Scanner sc = new Scanner(System.in);
-    public String path = "filesCSV/cursos.csv";
-    File arquivo = new File(path);
-    List<Curso> cursos = LeitorCursos.getCursos();
-    
-    public void novoCurso() {
+	Scanner sc = new Scanner(System.in);
+	public String path = "filesCSV/cursos.csv";
+	File arquivo = new File(path);
+	List<Curso> cursos = LeitorCursos.getCursos();
 
-        System.out.println("Digite o nome do curso");
-        String nome = sc.next();
-        System.out.println("Digite o nivel do curso (POS_GRADUACAO ou GRADUACAO)");
-        String nivel = sc.next();
-        System.out.println("Digite o ano do curso");
-        String ano = sc.next();
-        int x = 0;
-        
-        for(Curso curso: cursos ) {
-			if(curso.getNome().equals(nome) && curso.getTipo().equals(nivel) && curso.getAno().equals(ano)) {
-				x = 1;
+	public void novoCurso() {
+		try {
+			System.out.println("Digite o nome do curso");
+			String nome = sc.next();
+			System.out.println("Digite o nivel do curso (POS_GRADUACAO ou GRADUACAO)");
+			String nivel = sc.next();
+			System.out.println("Digite o ano do curso");
+			String ano = sc.next();
+
+			for (Curso curso : cursos) {
+				if (curso.getNome().equals(nome) && curso.getTipo().equals(nivel) && curso.getAno().equals(ano)) {
+					throw new DomainException("Curso Existente, tente novamente");
+				}
 			}
+
+			if (nivel.equals("GRADUACAO")) {
+				arquivo = new File("filesCSV/GraduacaoCSV/" + nome + "_" + nivel + "_" + ano + ".csv");
+			} else if (nivel.equals("POSGRADUACAO")) {
+				arquivo = new File("filesCSV/PosGraduacaoCSV/" + nome + "_" + nivel + "_" + ano + ".csv");
+			}
+			try {
+				arquivo.createNewFile();
+			} catch (IOException e) {
+
+			}
+
+			String[] lines = new String[] { nome + ";" + nivel + ";" + ano };
+
+			try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {
+				for (String line : lines) {
+					bw.write(line);
+					bw.newLine();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		} catch (DomainException r) {
+			System.out.println(r.getMessage());
 		}
-        
-        if(x == 0) {
-        if (nivel.equals("GRADUACAO")) {
-            arquivo = new File("filesCSV/GraduacaoCSV/" + nome + "_" + nivel + "_" + ano + ".csv");
-        } else if (nivel.equals("POSGRADUACAO")) {
-            arquivo = new File("filesCSV/PosGraduacaoCSV/" + nome + "_" + nivel + "_" + ano + ".csv");
-        }
-        try {
-            arquivo.createNewFile();
-        } catch (IOException e) {
-
-            }
-
-            String[] lines = new String[] { nome + ";" + nivel + ";" + ano };
-
-            try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {
-                for (String line : lines) {
-                    bw.write(line);
-                    bw.newLine();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        else {
-        	System.out.println("Curso já existente");
-        }
-    }
+	}
 }
-
